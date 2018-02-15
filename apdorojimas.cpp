bb30f8b321193ed785b16d9aeb9dac996f8888cf
unknown <benas.puck@gmail.com>
2018-02-15 11:02:16 +0200

    prideta mediana

--- apdorojimas.cpp
+++ apdorojimas.cpp
@@ -6,17 +6,23 @@ using std::cout;
 using std::string;
 using std::ifstream;
 using std::setprecision;
+using std::endl;
 int main()
 {
     ifstream fd("duom.txt");
     string vardas;
     string pavarde;
     int n;
+    int k;
     fd>>n;
     double balai;
     double vidurkis=0;
     double galutinis;
     double egzaminas;
+    cout<<"kaip vertinsime?"<<endl<<"1 - vidurkis   2 - mediana"<<endl;
+    cin>>k;
+    if(k==1)
+    {
     for(int i=0; i<n;i++)
     {
         fd>>balai;
@@ -25,6 +31,16 @@ int main()
     fd>>egzaminas;
     galutinis=0.4*(vidurkis/n)+0.6*egzaminas;
     cout<<setprecision(3)<<galutinis;
+    }
+    if(k==2)
+    {
+        for(int i=0; i<n;i++)
+    {
+        fd>>balai;
+        if(n/2==i)
+            cout<<balai<<endl;
+    }
+    }
     fd.close();
     return 0;
 
